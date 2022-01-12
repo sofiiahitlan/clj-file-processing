3a498ff497b155f1fcc93c4ec403bddab717e674
Sofiia Hitlan <s_hitlan@mail.harpercollege.edu>
2022-01-11 20:47:36 -0600

    Rough draft w/ dev comments

--- src/clj_file_processing/core.clj
+++ src/clj_file_processing/core.clj
@@ -1,10 +1,59 @@
 (ns ^{:authors "Sofia Hitlan"}
     clj-file-processing.core
   ;;(:gen-class)
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io])
   )
 
+(comment "Decide on how to process files. Have the atom and swap the value each
+new file is processed maybe? If no, figure out how to use transducer.")
+
+(def files-to-process
+  ["mock_data_commas.csv" "mock_data_pipelines.csv" "mock_data_whitespace.csv"])
+
+(defn process-file
+  "Opens file using with-open to ensure the Reader is properly closed when
+  processing is completed."
+  [transducer file]
+  (let [user-dir (str (System/getProperty "user.dir") "/csv")
+        file-path (str user-dir file)]
+    (try
+      (with-open [reader (io/reader file-path)]
+        (into []
+              transducer
+              (line-seq reader)))
+      (catch Exception e
+        (println "clj-file-processing.core/process-file error reading file:"
+                 [file-path e])))))
+
+(defn process-files
+  [transducer files]
+  (into
+   []
+   (comp (mapcat (fn [file] (process-file transducer file))))
+   files))
+
+(defn process-data
+  "Removes header (if present). Output is vector of vectors."
+  [output]
+  (let [output* (into []
+                      (for [entry output]
+                        (case delimiter
+                          ;; TODO process based on the delimiter type.
+                          ;; check for type once for efficiency?
+                          ;; write to another file or str?
+                          )
+                        ))]))
+
+
 (defn -main
-  "TODO: add docstring"
+  "Processes multiple files with a transducer as if it's one file. Not lazy, but
+  processes a line at a time. Checks that there is at least one file to be
+  processed. TODO: trim output, split by new line, process each line one by one?
+  Store as vector of vectors maybe."
   [& args]
-  (println "Hello, World!"))
+  (let [output (atom nil)])
+  (when (seq files-top-process)
+    (let [output (apply + (process-files (comp (map count))
+                                         files-to-process))
+          ])))
