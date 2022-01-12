eeb66e64c095d0e715783f5ae63df8f27bbb5cc3
Sofiia Hitlan <s_hitlan@mail.harpercollege.edu>
2022-01-11 19:37:50 -0600

    Project files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-file-processing
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clj_file_processing/core.clj
@@ -0,0 +1,10 @@
+(ns ^{:authors "Sofia Hitlan"}
+    clj-file-processing.core
+  ;;(:gen-class)
+  (:require [clojure.string :as str])
+  )
+
+(defn -main
+  "TODO: add docstring"
+  [& args]
+  (println "Hello, World!"))
